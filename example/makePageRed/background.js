501ec73068b15c9a681ceef57634242b44531585
mvip <834041690@qq.com>
2013-05-17 14:43:42 +0800

    Make the Page Red Over

---
+++ example/makePageRed/background.js
@@ -0,0 +1,7 @@
+chrome.browserAction.onClicked.addListener(function(tab){
+	//No tabs or host permissions needed!
+	console.log('Turning ' + tab.url + ' red!');
+	chrome.tabs.executeScript({
+		code: 'document.body.style.backgroundColor="green"'
+	});
+});
